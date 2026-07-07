101eb670a9543d4b899c95dd78ce5fe777baeea5
Md Sadiul Haque <54974583+mdsadiulhaque@users.noreply.github.com>
2026-07-07 16:19:45 +0600

    Add student dictionary with personal details

---
+++ Chapter 03 — Data Types/Mini Project.py
@@ -0,0 +1,12 @@
+student = {
+    "name":"Md Sadiul Haque",
+    "age":24,
+    "country":"Bangladesh",
+    "skills":[
+        "Python",
+        "SQL",
+        "Power BI"
+    ]
+}
+
+print(student)
